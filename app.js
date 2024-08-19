ed92e0bba4650df9a10a5aed3935c618f5ac8e50
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-08-19 10:53:27 +0300

    Create routes and controllers directores

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const tasks = require("./routes/tasks");
 const port = 3000;
 const app = express();
+
+// Middleware
+app.use(express.json());
+
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("<h1>Task Manger App!</h1>")
+})
+
+app.use("/api/v1/tasks", tasks);
 
 app.listen(port, () => {
   console.log(`Server rup and running on port ${port} localhost:3000`);
