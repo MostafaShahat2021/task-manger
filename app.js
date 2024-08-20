7a89335447d3df1bfac0af1f401f8f4b965af0ba
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-08-20 12:51:27 +0300

    Udate connectDB function with log statment

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect")
-const port = 3000;
+const port = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI
 const app = express();
 
@@ -21,7 +21,7 @@ app.use("/api/v1/tasks", tasks);
 const start = async () => {
   try {
     await connectDB(MONGODB_URI)
-    app.listen(port, () => console.log(`Server rup and running on port ${port} localhost:3000`));
+    app.listen(port, () => console.log(`Server up and running on port ${port} localhost:3000`));
 
   } catch (error) {
     console.log(error);
--- db/connect.js
+++ db/connect.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 const connectDB = (url) => {
+  console.log("MongoDB Connected");
   return mongoose.connect(url)
 }
 module.exports = connectDB;
+//1:11 youtube
