6450a950cbc28a1f8b3b27ad946aed0fb2d39f7e
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-08-20 14:30:39 +0300

    Update geAlltTasks & createTask func with try catch on controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,13 +1,21 @@
 const Task = require("../models/Task")
 
 const getAllTasks = async(req, res) => {
-  const tasks = await Task.find()
+  try {
+    const tasks = await Task.find()
   res.status(200).json(tasks)
+  } catch (error) {
+    res.status(400).json({message: error.message})
+  }
 }
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body) 
-  res.status(201).json({task})
+  try {
+    const task = await Task.create(req.body) 
+    res.status(201).json(task)
+  } catch (error) {
+    res.status(500).json({message: error.message})
+  }
 }
 
 const getTask = async (req, res) => {
