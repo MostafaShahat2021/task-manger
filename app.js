a6b877d6a45cea2e58de2fe7e6bd2d3a44281d5c
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-08-21 08:55:37 +0300

    Add errorHandlerMiddleware as a custom error handler

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ const port = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI
 const app = express();
 const notFound = require("./middleware/notFound")
+const errorHandlerMiddleware = require("./middleware/errorHandler")
 
 
 // Middleware
 app.use(express.json());
-app.use(express.static("./public"))
+app.use(express.static("./public"));
 
 // Routes
 app.use("/api/v1/tasks", tasks);
 app.use(notFound)
+app.use(errorHandlerMiddleware)
 
 const start = async () => {
   try {
